47d3f208154269cf119778ab3bc7c8fe5448663a
Fufu <fufuok@qq.com>
2021-11-04 17:11:14 +0800

    Update: create all indices

--- main.py
+++ main.py
@@ -8,11 +8,14 @@
     :author: Fufu, 2021/7/23
     :update: Fufu, 2021/8/21 调整结构, 新建索引时参考昨天的索引而不是今天
     :update: Fufu, 2021/9/18 超时时间增加到 240s, 创建索引失败记录并重试
+    :update: Fufu, 2021/11/3 超时时间统一为 300s, 保存最新的 MAPPING, 补齐配置中可能的索引
 """
+import json
 import os
 import sys
 import time
 from datetime import datetime, timedelta
+from hashlib import md5
 
 import requests as requests
 from elasticsearch import Elasticsearch
@@ -20,6 +23,7 @@ from envcrypto import get_environ
 from loguru import logger
 
 ROOT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+MAPPING_FILE = os.path.join(ROOT_DIR, 'etc', 'all_indices_mapping.json')
 ES = None
 
 
@@ -47,7 +51,7 @@ def init_es(hosts):
     """连接 ES"""
     for i in range(5):
         try:
-            return Elasticsearch(hosts, timeout=180)
+            return Elasticsearch(hosts, timeout=300)
         except Exception as e:
             logger.error('INIT: ({}) {}', i, e)
 
@@ -56,6 +60,8 @@ def init_es(hosts):
 
 def create_new_indexs():
     """新建索引"""
+    mapping = load_mapping()
+    indices = {}
     today = datetime.now()
     yesterday = today + timedelta(days=-1)
     tomorrow = today + timedelta(days=1)
@@ -68,31 +74,69 @@ def create_new_indexs():
         pos = len(a)
         # 取昨天的索引配置
         for index, conf in ES.indices.get('*' + a).items():
+            index_title = index[:-pos]
             # 移除必要配置项
             for x in remove_settings:
                 conf['settings']['index'].pop(x)
-            index_b = index[:-pos] + b
+            # 保存最新索引配置项
+            mapping[index_title] = conf
+            indices[index_title] = True
             # 建明天的索引
+            index_b = index_title + b
             if not create_index(index_b, conf):
                 retries_indexs.append([index_b, conf])
+
+    # 补漏, 可能存在于待删除列表
+    index_ymd = tomorrow.strftime('_%y%m%d')
+    for index_title in get_index_conf():
+        if index_title in indices:
+            continue
+        conf = mapping.get(index_title, {})
+        index_b = index_title + index_ymd
+        if not create_index(index_b, conf):
+            retries_indexs.append([index_b, conf])
 
     for x in retries_indexs:
         create_index(*x)
 
+    # 保存最新的 MAPPING
+    save_mapping(mapping)
+
+
+def load_mapping():
+    """加载保存的索引配置"""
+    try:
+        with open(MAPPING_FILE, 'r') as f:
+            res = json.load(f)
+            return res
+    except Exception as e:
+        logger.error('LOAD MAPPING: {}', e)
+        return {}
+
+
+def save_mapping(mapping=None):
+    """保存索引配置"""
+    try:
+        with open(MAPPING_FILE, 'w') as f:
+            json.dump(mapping, f, ensure_ascii=False, indent=2)
+    except Exception as e:
+        logger.error('SAVE MAPPING: {}', e)
+
 
 def create_index(index_b, conf):
     """建明天的索引"""
+    logger.info('NEW-START: {}', index_b)
     try:
-        ES.indices.create(index_b, body=conf, timeout='240s', ignore=400)
+        ES.indices.create(index_b, body=conf, timeout='300s', ignore=400)
     except Exception as e:
-        logger.error('NEW: {} {}', index_b, e)
-        time.sleep(120)
+        logger.error('NEW-ERROR: {} {}', index_b, e)
+        time.sleep(60)
         return False
 
     # 索引创建时间
     creation_date = ES.indices.get(index_b).get(index_b, {}). \
         get('settings', {}).get('index', {}).get('creation_date')
-    logger.info('NEW: {} {}', index_b,
+    logger.info('NEW-END: {} {}', index_b,
                 datetime.fromtimestamp(int(creation_date) / 1000).isoformat() if creation_date else 'None')
     time.sleep(30)
     return True
@@ -144,10 +188,10 @@ def delete_old_indexs():
         if n > 0:
             old_index = '{}_{}'.format(index, (datetime.now() - timedelta(days=n)).strftime('%y%m%d'))
             try:
-                ES.indices.delete(old_index, timeout='240s', ignore=[400, 404])
+                ES.indices.delete(old_index, timeout='300s', ignore=[400, 404])
             except Exception as e:
                 logger.error('DELETE: {} {}', old_index, e)
-                time.sleep(60)
+                time.sleep(30)
                 continue
 
             logger.info('DELETE: {}', old_index)
